a6d6056ace5d5cd733c9be4a2fffb0ad379799ed
klaudm <58596509+klaudm@users.noreply.github.com>
2019-12-21 22:30:09 +0100

    getline test

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
-
 int main()
 {
-	cout << "Welcome to Bulls and Cows\n";
+	// introduce the game
+	constexpr int WORLD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORLD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	// get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
+
+	getline(cin, Guess);
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+	// get a guess from the player
+	cout << "Enter your guess: ";
+	cin >> Guess;
+
+	getline(cin, Guess);
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+	cout << endl;
 	return 0;
 }
