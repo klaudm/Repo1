03b2f6bf87fd88bcd4be1b1cf58c85837b7d6cde
klaudm <58596509+klaudm@users.noreply.github.com>
2019-12-24 18:18:44 +0100

    FINAL

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -37,6 +37,12 @@ int main()
 void PrintIntro()
 {
 	std::cout << "\n\nWelcome to Bulls and Cows, a fun word game.\n";
+	std::cout << std::endl;
+	std::cout << " ((_, ..., _)) " << std::endl;
+	std::cout << "    | o o | " << std::endl;
+	std::cout << "     \\\\ // " << std::endl;
+	std::cout << "      ^_^ " << std::endl;
+	std::cout << std::endl;
 	std::cout << "Can you guess the " << BCGame.GetHiddenWordLength();
 	std::cout << " letter isogram I'm thinking of?\n";
 	std::cout << std::endl;
@@ -73,26 +79,26 @@ FText GetValidGuess()
 	do {
 		// get a guess from the player
 		int32 CurrentTry = BCGame.GetCurrentTry();
-		std::cout << "Try " << CurrentTry << ". Enter your guess: ";
+		std::cout << "Try " << CurrentTry << " of " << BCGame.GetMaxTries();
+		std::cout << ". Enter your guess: ";
 		std::getline(std::cin, Guess);
 
 		// check status and give feedback
 		Status = BCGame.CheckGuessValidity(Guess);
 		switch (Status) {
 		case EGuessStatus::Wrong_Length:
-			std::cout << "Please enter a " << BCGame.GetHiddenWordLength() << " letter word.\n";
+			std::cout << "Please enter a " << BCGame.GetHiddenWordLength() << " letter word.\n\n";
 			break;
 		case EGuessStatus::Not_Isogram:
-			std::cout << "Please enter a word without repeating letters.\n";
+			std::cout << "Please enter a word without repeating letters.\n\n";
 			break;
 		case EGuessStatus::Not_Lowercase:
-			std::cout << "Please enter a all lowercase letters.\n";
+			std::cout << "Please enter a all lowercase letters.\n\n";
 			break;
 		default:
 			// assume the guess is valid
 			break;
 		}
-		std::cout << std::endl;
 	} while (Status != EGuessStatus::OK); // keep looping until we get no errors
 	return Guess;
 }
@@ -109,7 +115,9 @@ void PrintGameSummary()
 {
 	if (BCGame.IsGameWon())
 	{
+		std::cout << "~^~^~^~^~^~^~^~^~^~^~\n";
 		std::cout << "WELL DONE - YOU WIN!\n";
+		std::cout << "~^~^~^~^~^~^~^~^~^~^~\n\n";
 	}
 	else
 	{
